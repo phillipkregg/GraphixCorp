a47e5a2eaf052eb71257eb87720603bab7804d7b
Your Name <phillipkregg@gmail.com>
2012-10-24 16:06:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphixcorp::Application.config.secret_token = '5a496bf35a749132de50e4e51e2f5d6d9fd29e870a6783cf8cc2050c8268b9828d5a45005690c0018ed11beaba6f9ce47cdc65f2a4df91db542e0aed90519726'
